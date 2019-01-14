e23b93c965c6c3600f66d9b7309163ed0190536c
unknown <richwhat89@gmail.com>
2019-01-14 08:51:11 -0600

    morning commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+import Dashboard from './components/Dashboard';
+import Home from './components/Home';
+import Register from './components/Register';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Router>
+        <div>
+          <Route path='/dashboard' exact render={()=>this.props.user.username ? <Dashboard/>: <Redirect to='/'/>}/>
+          <Route path='/register' exact component={Register}/>
+          <Route path='/' exact component={Login}/>
+        </div>
+      </Router>
     );
   }
 }
